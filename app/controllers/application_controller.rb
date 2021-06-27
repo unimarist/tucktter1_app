b93346858c938a60a1d61ee5f93ea6b3f3816afb
unimarist <75885767+unimarist@users.noreply.github.com>
2021-06-27 20:01:31 +0900

    ユーザ管理機能実装
    以下を実施
    ・Usersテーブル作成
    ・ログイン/ログアウトの実装
    ・アカウント情報編集の実装
    ・Active Storageの導入
    ・ストロングパラメータ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    added_attrs = [:email,:LastName,:FirstName,:birthday,:aWord,:favoriteSubject,:Nickname,:student_or_cauch,:university,:department,:major,:status_identification, :user_icon, :password, :password_confirmation, :remember_me, :user_identification]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
