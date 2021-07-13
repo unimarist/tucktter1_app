c0501d070cb0c5e01d0f1daa5900c3eecd6fdb98
unimarist <75885767+unimarist@users.noreply.github.com>
2021-07-13 14:37:35 +0900

    「My呟き」画面からも呟けるように設定
    user_controllerのshowメソッド内に、tweetインスタンス作成の記述を追記

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,7 @@ class UserController < ApplicationController
   def show
     @nickname = current_user.Nickname
     @tweets = current_user.tweets.order(created_at: :desc)
+    @tweet = Tweet.new
   end
 
 end
