8a9254fc817a0243ae96d9dfa11a394c425abcfd
unimarist <75885767+unimarist@users.noreply.github.com>
2021-07-13 15:06:47 +0900

    バリデーションチェック・エラー表示設定
    研究投稿において、必須項目が入力されていない等、バリデーションチェックに引っかかった場合、エラーメッセージが表示されるように設定

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -13,7 +13,7 @@ class ResearchesController < ApplicationController
     @research = Research.create(research_params)
     if @research.save
     else
-      flash.now[:error_research] = "各入力項目は必須です。研究タイトルは50字以内、研究概要は500文字以内で入力して下さい。"
+      flash.now[:error_research] = "各入力項目は必須です。研究タイトルは50字以内、研究概要は500字以内で入力して下さい。"
       render :new
     end
   end
