fd5bd901970762d92b84ab2e2ef4410e89304fff
unimarist <75885767+unimarist@users.noreply.github.com>
2021-07-17 13:57:03 +0900

    いいね機能_研究(メソッド作成)
    研究のいいね投稿用メソッド(create)を作成

--- app/controllers/research_likes_controller.rb
+++ app/controllers/research_likes_controller.rb
@@ -1,2 +1,13 @@
 class ResearchLikesController < ApplicationController
+  def create
+    Researchlike.create(like_params)
+    redirect_to research_path(like_params[:research_id]) 
+  end
+
+  private
+
+  def like_params
+    params.require(:research_like).merge(user_id: current_user.id, research_id: params[:research_id])
+  end
+
 end
