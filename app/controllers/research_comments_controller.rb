b0df7cb86732c74400adb8d199587b19271383ba
unimarist <75885767+unimarist@users.noreply.github.com>
2021-07-17 12:06:20 +0900

    コメント機能_研究(メソッド作成)
    研究のコメント投稿用メソッド(create)を作成

--- app/controllers/research_comments_controller.rb
+++ app/controllers/research_comments_controller.rb
@@ -1,2 +1,14 @@
 class ResearchCommentsController < ApplicationController
+
+  def create
+    Comment.create(comment_params)
+    redirect_to "/researchs/#{comment_params[:research_id]}"
+  end
+
+  private
+
+  def comment_params
+    params.require(:research_comment).permit(:text).merge(user_id: current_user.id, research_id: params[:research_id])
+  end
+
 end
