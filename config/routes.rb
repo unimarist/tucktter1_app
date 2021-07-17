3649a0009c093d90f582f95182de8364c88b56ca
unimarist <75885767+unimarist@users.noreply.github.com>
2021-07-17 11:58:12 +0900

    コメント機能_研究(ルーティング設定)
    研究用のコメント機能について、コメント投稿のルーティング設定を実施

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :user, only: :show
   resources :operations , only: [:index,:edit]
   resources :researches do
+    resources :comments, only: :create
     collection do
       get 'my_research'
       get 'search'
