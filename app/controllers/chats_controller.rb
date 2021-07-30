5185af8ffb81d7a11fb864debc5b045f0f39e730
sam-19950315 <isamucap@gmail.com>
2021-07-30 21:10:59 +0900

    chatroom内のメッセージ一覧表示機能実装
    chatroom内のメッセージ一覧表示機能実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,8 @@
 class ChatsController < ApplicationController
   def index
     binding.pry
-    @room_name = ChatRoom.find(params[:room_name])
+    @chat_room = ChatRoom.find(params[:chat_room_id])
     @chat = Chat.new
-    @chats = @room_name.chats.include(:user)
+    @chats = @chat_room.chats.include(:user)
   end
 end
